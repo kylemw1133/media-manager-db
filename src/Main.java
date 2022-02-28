0c46fcdbe88b4e3f5fc4d53e79eea88c8c6293a5
Kyle Williams <kylemw1133@gmail.com>
2022-02-28 11:51:56 -0500

    added command ui input

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,40 @@
+import java.util.Scanner;
+
 import commands.*;
 import data.*;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		String command = "add";
-		
-		switch (command) {
-		case "add":
-			AddCommand.exec(args);
-			break;
-		case "edit":
-			EditCommand.exec(args);
-			break;
-		case "search":
-			SearchCommand.exec(args);
-			break;
-		case "order":
-			OrderCommand.exec(args);
-			break;
-		case "report":
-			ReportCommand.exec(args);
-			break;
-		default:
-			System.out.println("Invalid subcommand: " + command);
+		boolean promptUser = true;
+		Scanner s = new Scanner(System.in);
+		while(promptUser) {
+			System.out.println("Enter db command:");
+			String command = s.next();
+			switch (command) {
+			case "add":
+				AddCommand.exec(args);
+				break;
+			case "edit":
+				EditCommand.exec(args);
+				break;
+			case "search":
+				SearchCommand.exec(args);
+				break;
+			case "order":
+				OrderCommand.exec(args);
+				break;
+			case "report":
+				ReportCommand.exec(args);
+				break;
+			case "exit":
+				promptUser = false;
+				break;
+			default:
+				System.out.println("Invalid subcommand: " + command);
+			}
 		}
-	}
+		s.close();
+	}	
 
 }
