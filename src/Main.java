cebf500e44bc3f6cd933a482469a693fcd640bdb
Sichan Yuh <sichan.yuh@gmail.com>
2022-03-01 14:49:40 -0500

    stash

--- src/Main.java
+++ src/Main.java
@@ -4,37 +4,37 @@ import commands.*;
 import data.*;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		boolean promptUser = true;
 		Scanner s = new Scanner(System.in);
-		while(promptUser) {
+		while (promptUser) {
 			System.out.println("Enter db command:");
 			String command = s.next();
 			switch (command) {
-			case "add":
-				AddCommand.exec(args);
-				break;
-			case "edit":
-				EditCommand.exec(args);
-				break;
-			case "search":
-				SearchCommand.exec(args);
-				break;
-			case "order":
-				OrderCommand.exec(args);
-				break;
-			case "report":
-				ReportCommand.exec(args);
-				break;
-			case "exit":
-				promptUser = false;
-				break;
-			default:
-				System.out.println("Invalid subcommand: " + command);
+				case "add":
+					AddCommand.exec(args);
+					break;
+				case "edit":
+					EditCommand.exec(args);
+					break;
+				case "search":
+					SearchCommand.exec(args);
+					break;
+				case "order":
+					OrderCommand.exec(args);
+					break;
+				case "report":
+					ReportCommand.exec(args);
+					break;
+				case "exit":
+					promptUser = false;
+					break;
+				default:
+					System.out.println("Invalid subcommand: " + command);
 			}
 		}
 		s.close();
-	}	
+	}
 
 }
