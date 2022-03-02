b8cc7d653db064a3873bd162ec5eb488e482320f
Tassos Manganaris <tassos.manganaris@gmail.com>
2022-03-01 23:58:13 -0500

    Added example report

--- src/commands/ReportCommand.java
+++ src/commands/ReportCommand.java
@@ -1,9 +1,32 @@
 package commands;
 
+import java.util.ArrayList;
+
+import data.FakeDatabase;
+import entities.Album;
+import entities.InventoryItem;
+
 public class ReportCommand {
 
 	public static void exec() {
-		System.out.println("report logic goes here");
+		System.out.println("Example report: all albums with length greater than 50.");
+
+		ArrayList<Album> validAlbums = new ArrayList<Album>();
+		for (InventoryItem i : FakeDatabase.getInstance().inventoryItems) {
+			if (i instanceof Album) {
+				Album a = (Album) i;
+				if (a.length > 50) {
+					validAlbums.add(a);
+				}
+			}
+		}
+		
+		if (validAlbums.isEmpty()) {
+			System.out.println("No items found.\n");
+			return;
+		}
+		
+		System.out.println(validAlbums);
 	}
 
 }
